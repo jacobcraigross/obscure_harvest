c6b7f925f405459f954cd2c5ed96e1ea807f6db3
Jacob Ross <survivaldawn@gmail.com>
2019-09-22 06:28:29 -0500

    pounds to kilos

--- app.py
+++ app.py
@@ -1,5 +1,19 @@
-# an expression
-print("*" * 1000)
+# pounds to kilos. 
+weight_lbs = input('how much do you weigh in pounds? ')
+weight_kg = int(weight_lbs) * 0.45
+print('your weight in kilograms is ---> ',weight_kg)
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
